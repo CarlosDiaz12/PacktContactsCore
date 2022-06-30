df172260c4a5a1061c6d1cb03c149ed26b1a13d7
Carlos Diaz <carlos.cedp@gmail.com>
2022-06-30 12:52:16 -0400

    initial setup

---
+++ Data/ContactsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PacktContactsCore.Models;
+
+namespace PacktContactsCore.Data
+{
+    public class ContactsDbContext : DbContext
+    {
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> contextOptions) : base(contextOptions) { }
+        public ContactsDbContext() { }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PacktContactsCore.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
